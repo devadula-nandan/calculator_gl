07fcb196678ab500d6ed002389d65b907d811633
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2021-12-10 12:51:24 +0530

    Update calculatorScript.js

--- calculatorScript.js
+++ calculatorScript.js
@@ -73,11 +73,12 @@ const performOperation = (num1, num2) => {
 	}
 	_result = String(_result);
 	if (_result === "Infinity") return clearScreen("Can't divide by", "zero");
-	if !_result.includes("."){
+	if _result.includes("."){
+		if (_result.split('.')[0].length <= 8) return _result.split('.')[0] + "." + (_result.split('.')[1].length > 8 - _result.split('.')[0].length  ? _result.split('.')[1][8-_result.split('.')[0].length] :_result.split('.')[1] ;)
+		
+	} else {
 		if (_result.length > 9) return (clearScreen("Range", "Error") , console.log(_result));
 		return _result;
-	} else {
-		if (_result.split('.')[0].length <= 8) return _result.split('.')[0] + "." + (_result.split('.')[1].length > 8 - _result.split('.')[0].length  ? _result.split('.')[1][8-_result.split('.')[0].length] :_result.split('.')[1] ;)
 	}
 	
 };
