7971aad71f00c95e33a7c6c976772ab1b4e6810b
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2021-12-11 00:57:05 +0530

    Update calculatorScript.js

--- calculatorScript.js
+++ calculatorScript.js
@@ -131,7 +131,7 @@ const handleClick = (buttonName) => {
 		}
 	} else if(["%" , "x" , "÷"].includes(`${expression.innerHTML[expression.innerHTML.length-1]}`) && ["+","-"].includes(buttonName)){
 		result.innerHTML = buttonName;
-	} else if(["+","-"].includes(buttonName) && result.innerHTML.length == 1){
+	} else if(["+","-"].includes(buttonName) && result.innerHTML == "0"){
 		result.innerHTML = buttonName;
 	}
 	else handleOperator(buttonName);
